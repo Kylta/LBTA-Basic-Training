9ce0df4c424449d12265dbfcdb291fffbd30a2fe
Kylta <Christophe.bugnon02@gmail.com>
2018-02-27 20:29:18 +0100

    Finish 3. Custom Cell Layout with : MVC and Encapsulation

--- LBTA - Basic training/LBTA - Basic training/Controller/ViewController.swift
+++ LBTA - Basic training/LBTA - Basic training/Controller/ViewController.swift
@@ -26,23 +26,19 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80
+        return 86
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! BookCell
         
         let book = books?[indexPath.row]
-        
-        cell.textLabel?.text = book?.title
-        cell.imageView?.image = book?.image
+        cell.book = book
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
         guard let count = books?.count else { return 0 }
         
         return count
@@ -66,4 +62,6 @@ class ViewController: UITableViewController {
     }
     
 }
+
+
 
--- LBTA - Basic training/LBTA - Basic training/View/BookCell.swift
+++ LBTA - Basic training/LBTA - Basic training/View/BookCell.swift
@@ -9,9 +9,55 @@
 import UIKit
 
 class BookCell: UITableViewCell {
+    
+    var book: Book? {
+        didSet {
+            coverImageView.image = book?.image
+            titleLabel.text = book?.title
+            authorLabel.text = book?.author
+        }
+    }
+    
+    private let coverImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "There is text for title"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let authorLabel: UILabel = {
+        let label = UILabel()
+        label.text = "There is text for authord"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(coverImageView)
+        coverImageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 8).isActive = true
+        coverImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        coverImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -8).isActive = true
+        coverImageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        addSubview(titleLabel)
+        titleLabel.leftAnchor.constraint(equalTo: coverImageView.rightAnchor, constant: 8).isActive = true
+        titleLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
+        titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -10).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        
+        addSubview(authorLabel)
+        authorLabel.leftAnchor.constraint(equalTo: coverImageView.rightAnchor, constant: 8).isActive = true
+        authorLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4).isActive = true
+        authorLabel.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
+        authorLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
         
     }
     
